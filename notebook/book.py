90195c336700d54f5c24c292adea3ddbdaa5be18
Sameer Adhikari <tri2sing@yahoo.com>
2015-09-01 22:58:28 -0700

    Chapter 2. My version of the case study.

---
+++ notebook/book.py
@@ -0,0 +1,93 @@
+'''
+Created on Aug 31, 2015
+
+@author: Sameer Adhikari
+'''
+from notebook.note import Note
+
+class Notebook(object):
+    '''
+    A collection that allows user to:
+    1. Create a new note
+    2. Modify the text of an existing note
+    3. Tag a note
+    4. Search inside the note text and tags
+    '''    
+
+    def __init__(self):
+        '''
+        Initialize a notebook with an empty list of notes
+        '''
+        self.notes_list = []
+        
+    def add_new_note(self, note_text, note_tags=''):
+        '''Create a new note and add to notebook.
+        '''
+        self.notes_list.append(Note(note_text=note_text, note_tags=note_tags))
+        
+    def _find_note(self, note_id):
+        '''Returns the note with id that matches given one.  
+        Currently performs an inefficient linear scan.
+        '''
+        for note in self.notes_list:
+            if note.note_id == note_id:
+                return note
+        return None
+
+    def get_num_notes(self):
+        '''Return the number of notes
+        '''
+        return len(self.notes_list)
+    
+    def get_note_text(self, note_id):
+        '''Find note with the given id and 
+        return its text
+        '''
+        note = self._find_note(note_id=note_id)  
+        return note.note_text
+    
+    def replace_note_text(self, note_id, note_text):
+        '''Find note with the given id and replace
+        its text with the given text
+        '''
+        note = self._find_note(note_id=note_id)  
+        note.note_text = note_text
+        
+    def get_note_tags(self, note_id):
+        '''Find note with the given id and 
+        return its text
+        '''
+        note = self._find_note(note_id=note_id)  
+        return note.note_tags
+    
+    def replace_note_tags(self, note_id, note_tags):
+        '''Find note with the given id and replace
+        its tags with the given tags
+        '''
+        note = self._find_note(note_id=note_id)  
+        note.note_tags = note_tags
+    
+    def find_notes(self, search_string):
+        '''Find all the notes that contain the search string
+        '''
+        return [note for note in self.notes_list if note.contains(search_string)]
+
+if __name__ == '__main__':
+    nb = Notebook()
+    nb.add_new_note('hello world', '')
+    nb.add_new_note('hello again', '')
+    # This for loop assumes that the note ids are 0..N-1
+    # Not really top-shelf code, but that is not the aim
+    for note_id in range(nb.get_num_notes()):
+        note_text = nb.get_note_text(note_id)
+        print('Note = {0}, Text = {1}'.format(note_id, note_text))
+
+    notes = nb.find_notes('hello')
+    for note in notes:
+        print(note.note_text)
+
+    nb.replace_note_text(0, 'hi world')
+    print(nb.get_note_text(0))
+    
+    nb.replace_note_tags(1, 'message, greeting')
+    print(nb.get_note_tags(1))
---
+++ notebook/note.py
@@ -0,0 +1,43 @@
+'''
+Created on Aug 30, 2015
+
+@author: Sameer Adhikari
+'''
+import datetime
+
+# the id available for the next note
+available_id = 0
+
+class Note(object):
+    '''Represent a note in the notebook.
+    Match against a string in searches.
+    Store tags for each note.
+    '''
+    def __init__(self, note_text, note_tags=''):
+        '''Initialize a note with a note_text and optional note_tags.
+        Automatically assign an id and creation date.
+        '''
+        global available_id
+        self.note_text = note_text
+        self.note_tags = note_tags
+        self.note_id = available_id
+        available_id += 1
+        self.creation_date = datetime.date.today()
+        return
+    
+    def contains(self, search_string):
+        '''Determines if the note contains the search_string.
+        Check both the note_text and note_tags for the presence 
+        of the search_string as a sub string in the note.
+        The search is case-sensitive.
+        '''
+        return search_string in self.note_text or search_string in self.note_tags
+        
+
+if __name__ == '__main__':
+    n1 = Note('first hello')
+    n2 = Note('second_hello') 
+    print('n1 id = {0}'.format(n1.note_id))       
+    print('n2 id = {0}'.format(n2.note_id))      
+    print(n1.contains('hello')) 
+    print(n2.contains('again')) 
