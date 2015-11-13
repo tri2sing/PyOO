3b6c20c5673981b03df7fdb7594f0669789131da
Sameer Adhikari <tri2sing@yahoo.com>
2015-11-12 22:24:52 -0800

    Added a decorator for socket that echoes the message before send

--- patterns/decorators/server.py
+++ patterns/decorators/server.py
@@ -4,18 +4,36 @@ Created on Nov 6, 2015
 @author: Sameer Adhikari
 '''
 
+import argparse
 import socket
+
+class EchoSocket:
+    '''
+    Decorator that adds echoing capabilities to the socket class.
+    Technically, this should implement all the methods of socket.
+    The send method should have all the optional parameters.
+    But, comprehensiveness is not a goal of this example.
+    '''
+    def __init__(self, socket):
+        self.socket = socket
+    
+    def send(self, data):
+        print('Sending to destination: {0}, data = {1}'.format(self.socket.getpeername()[0], data))
+        
+    def close(self):
+        self.socket.close()
 
 class Server:
     '''
     '''
-    def __init__(self):
+    def __init__(self, echo=False):
+        self.echo = echo
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind(('localhost', 2401))
         self.server.listen(1)
         
     def respond(self, client):
-        response = input("Enter a value to send to client: ")
+        response = input("Enter an integer value to send to client: ")
         client.send(bytes(response))
         client.close()
         
@@ -23,12 +41,17 @@ class Server:
         try:
             while True:
                 client, add = self.server.accept()
+                if self.echo:
+                    client = EchoSocket(client)
                 self.respond(client)
         finally:
             self.server.close()
                 
     
 if __name__ == '__main__':
-    server = Server()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--echo', type=bool, default=False, help = 'whether to echo the send on the terminal')
+    args = parser.parse_args()
+    server = Server(args.echo)
     server.run()
     
